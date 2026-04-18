124e7a1c46fec76b4385cd3ebd981384fc5f0cec
SIMRANKUMARI11 <simranrauniyar387@gmail.com>
2026-04-18 22:11:32 +0530

    Create LC_225_StackUsingQueue.java

---
+++ LC_225_StackUsingQueue.java
@@ -0,0 +1,31 @@
+// LC 225 - Implement Stack using Queues
+
+import java.util.*;
+
+class MyStack {
+    Queue<Integer> q;
+
+    public MyStack() {
+        q = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        q.add(x);
+        int size = q.size();
+        for (int i = 0; i < size - 1; i++) {
+            q.add(q.remove());
+        }
+    }
+
+    public int pop() {
+        return q.remove();
+    }
+
+    public int top() {
+        return q.peek();
+    }
+
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
